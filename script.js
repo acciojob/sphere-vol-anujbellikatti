6e9423d62917e7fb7eaa28fec2845f720ce80f10
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-09-06 22:29:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
-    //Write your code here
-  
+    //Write your code here.
+	 const radius = parseFloat(document.getElementById('radius').value);
+	const vol =4/3*Math.PI*radius*radius*radius
+    document.getElementById('volume').value = vol.toFixed(4) ; 
+
+    return false;
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
